c720bae3ec5db2164a482145c17a09b0ca60930e
Takanori Sugita <sugitata.nr@gmail.com>
2022-09-27 10:15:42 +0900

    contains duplicate

---
+++ contains_duplicate.py
@@ -0,0 +1,15 @@
+# @see https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Simplest solution
+        # return len(nums) != len(set(nums))
+
+        # another solution
+        # @see https://leetcode.com/problems/contains-duplicate/discuss/1077092/leetcode-217-contains-duplicate-easy-python-solution
+        a = set()
+        for i in range(len(nums)):
+            if nums[i] not in a:
+                a.add(nums[i])
+            else:
+                return True
+        return False
