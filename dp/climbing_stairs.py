73d2fba04bebfe116f1220ed2f3db9ca2783893b
Takanori Sugita <sugitata.nr@gmail.com>
2022-10-15 11:12:56 +0900

    climbing stairs

---
+++ dp/climbing_stairs.py
@@ -0,0 +1,11 @@
+# @see https://zhenyu0519.github.io/2020/02/18/lc70/
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
