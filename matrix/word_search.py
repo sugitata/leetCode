8c416021eb8320dd160b82f7d694cfbe45907742
Takanori Sugita <sugitata.nr@gmail.com>
2022-12-08 10:15:56 +0900

    word search

---
+++ matrix/word_search.py
@@ -0,0 +1,51 @@
+# @see https://leetcode.com/problems/word-search/solutions/27820/Python-DFS-solution/
+# it is a question by using (DFS) 深さ優先探索 depth first search
+# https://qiita.com/drken/items/4a7869c5e304883f539b
+from typing import List
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        visited = {}
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.getWords(board, word, i, j, visited):
+                    return True
+
+        return False
+
+    def getWords(
+        self, board: List[List[str]], word: str, i: int, j: int, visited, pos: int = 0
+    ) -> bool:
+        # 今のポジションが探索している文字の長さと一致したなら操作終了
+        if pos == len(word):
+            return True
+
+        # i < 0 or i == len(board) : 範囲内のrowか確認
+        # j < 0 or j == len(board[0]) : 範囲内のcolumnか確認
+        # visited.get : DFSでseenになっているか確認
+        # word[pos] != board[i][j] : 今ターゲットにしているwordの一文字がboardの今seeしているものと合っているか
+        if (
+            i < 0
+            or i == len(board)
+            or j < 0
+            or j == len(board[0])
+            or visited.get((i, j))
+            or word[pos] != board[i][j]
+        ):
+            return False
+
+        # ? visitedってどういう型？
+        visited[(i, j)] = True
+        # ? なぜ or でresを取得する？
+        # -> 四方向で一つでもTrueになるものがあれば走査終了と見做せるから (pos == len(word))
+        res = (
+            self.getWords(board, word, i, j - 1, visited, pos + 1)
+            or self.getWords(board, word, i + 1, j, visited, pos + 1)
+            or self.getWords(board, word, i, j + 1, visited, pos + 1)
+            or self.getWords(board, word, i, j + 1, visited, pos + 1)
+        )
+        # ? なぜvisitedをFalseに戻す？
+        visited[(i, j)] = False
+
+        return res
