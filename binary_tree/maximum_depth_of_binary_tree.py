8edf0769dc50c53a39b1038617765509c0431985
Takanori Sugita <sugitata.nr@gmail.com>
2023-01-01 12:27:37 +0900

    maximum depth of binary tree

---
+++ binary_tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if not root:
+        #     return 0
+        # # BFS
+        # # @see https://qiita.com/drken/items/996d80bcae64649a6580
+        # # dictでやると、同じ数がある場合に詰む
+        # seen = {}
+        # seen[root.val] = 1
+        # q = [root]
+        # # dictのkeyをユニークにするため
+        # # [0,0,0,0,null,null,0,null,null,null,0] みたいな時に対応するため
+        # # length = 0
+        # while len(q) > 0:
+        #     v = q.pop(0)
+        #     # u_key = v.val + seen[v.val]
+        #     if seen[]
+        #     if v.left is not None:
+        #         # keyをユニークにさせる
+        #         # value + parentの階層
+        #         u_key = v.left.val + seen[v.val]
+        #         #
+        #         seen[u_key] = seen[v.val + seen[v.val]] + 1
+        #         q.append(v.left)
+        #     if v.right is not None:
+        #         u_key = v.right.val + seen[v.val]
+        #         seen[u_key] = seen[v.val] + 1
+        #         q.append(v.right)
+
+        # return max(seen.values())
+
+        # @see https://www.educative.io/answers/finding-the-maximum-depth-of-a-binary-tree
+        if root is None:
+            return 0
+
+        left_d = self.maxDepth(root.left)
+        right_d = self.maxDepth(root.right)
+
+        if left_d > right_d:
+            return left_d + 1
+        else:
+            return right_d + 1
