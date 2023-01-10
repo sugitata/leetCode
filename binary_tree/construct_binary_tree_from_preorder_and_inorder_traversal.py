2e9e5790a013daef52bed4df9557f1f031e02b82
Takanori Sugita <sugitata.nr@gmail.com>
2023-01-10 10:04:36 +0900

    add comment

--- binary_tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ binary_tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -17,9 +17,19 @@ class Solution:
         self, p: List[int], m: dict, pix: int, ileft: int, iright: int
     ) -> TreeNode:
         root_val = p[pix]
+        # rootを一番上から新規作成していく
+        # imid: inorderにおける、そのvaluenのindex
+        # ileft: splitTreeの 左側のindex
+        # iright: splitTreeの 右側のindex
+        # inorder: [ ileft..., imid, iright... ]
+        # 上記のように並んでいれば(ileft < imid < iright)、left, rightが存在することになるので、またrecursiveでsplitしていく
         root, imid = TreeNode(root_val), m[root_val]
         if imid > ileft:
+            # 次は [ileft ~ imid -1]の範囲で
+            # pixは ターゲットのindex インクリメントすれば良い
             root.left = self.splitTree(p, m, pix + 1, ileft, imid - 1)
         if imid < iright:
+            # 次は [imid + 1 + iright]の範囲で
+            # pixは ひとつ前にしたいので imid - ileft + 1
             root.right = self.splitTree(p, m, pix + imid - ileft + 1, imid + 1, iright)
         return root
